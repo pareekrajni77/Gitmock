d62bfa308f581b446c202e78b703eb4eeee83f17
pareekrajni77 <rajniparks@gmail.com>
2022-10-09 21:51:36 +0530

    lines added

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -1,7 +1,9 @@
 package org.computer;
 
 public class Desktop extends Computer {
-	
+	private void m1() {
+System.out.println("hello ffffff");
+	}
     public void desktopSize() {
         System.out.println("Desktop Size : 42 inch");
 		
